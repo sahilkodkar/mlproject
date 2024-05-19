2787769de4f84b1d4411cd5f2fab551a3c10f926
sahilkodkar <sahilakodkar@gmail.com>
2024-05-19 23:04:08 +0530

    Model Trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,7 +86,7 @@ class DataTransformation:
                 input_feature_test_df=test_df.drop(columns=[target_column_name], axis=1)
                 target_feature_test_df=test_df[target_column_name]
 
-                logging.info('Applying preprocessing object on training dataframe and testing dataframe.')
+                logging.info(f'Applying preprocessing object on training dataframe and testing dataframe.')
                 
                 input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
                 input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)
@@ -94,7 +94,7 @@ class DataTransformation:
                 train_arr=np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
                 test_arr=np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-                logging.info('Saved preprocessing object.')
+                logging.info(f'Saved preprocessing object.')
 
                 save_object(file_path=self.data_transformation_config.preprocessor_obj_file_path, obj=preprocessing_obj)
 
